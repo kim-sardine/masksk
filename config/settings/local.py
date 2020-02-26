3b10902e63ebd5f419f1ed60728f1813f401e5c4
kim-sardine <kim.sardine@gmail.com>
2020-02-26 19:41:08 +0900

    Add local dev settings for window docker

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="L25EegDg1XA4wgBytpBFqcv5vSBBc6mwH38WynSpmOS0bt1hD8esoYSl3MbJ69TS",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "192.168.99.100"]
 
 # CACHES
 # ------------------------------------------------------------------------------
@@ -42,7 +42,7 @@ DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TEMPLATE_CONTEXT": True,
 }
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
-INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2", "192.168.99.1"]
 if env("USE_DOCKER") == "yes":
     import socket
 
