a7c0a215f21459c18bc21ddaab9d5a5c4dc34d76
kim-sardine <kim.sardine@gmail.com>
2020-02-28 20:45:03 +0900

    Add crawler : welkeepsmall_all

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,11 @@ urlpatterns = [
 
     path(settings.ADMIN_URL, admin.site.urls),
 
-    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+    path("stores/", include("mask.stores.urls")),
 
-    # path("stores/", include("mask.stores.urls")),
+    # path(
+    #     "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+    # ),
 
     # path("users/", include("mask.users.urls", namespace="users")),
     # path("accounts/", include("allauth.urls")),
--- mask/stores/crawler.py
+++ mask/stores/crawler.py
@@ -13,12 +13,21 @@ def naver_smart_store_1(soup):
         return True
     return False
 
+# FIXME: 재고 있는 모습을 못봐서.. 아직 재고 있는 기준을 정하지 못함
 def welkeepsmall_1(soup):
     out_of_order = soup.find('div', class_='soldout')
-    print(out_of_order)
-    if out_of_order:
-        return False
-    return True
+    if not out_of_order:
+        return True
+    return False
+
+def welkeepsmall_all(soup):
+    masks = soup.find_all('div', class_="tb-center")
+    for mask in masks:
+        sold_out = mask.find('li', class_="soldout")
+        if not sold_out:  # 한개라도 sold out 이 아니면 재고 있음으로 판단
+            return True
+
+    return False
 
 def kakao_store_1(soup):
     bottom_button = soup.find('div', class_='_bottom_buttons wrap_btn_detail')
