8ea9f287cd5321dbfdd8f999200f0a3e57a7045f
kim-sardine <kim.sardine@gmail.com>
2020-02-28 14:32:40 +0900

    Add admin action to delete recent stock date

--- mask/stores/admin.py
+++ mask/stores/admin.py
@@ -11,9 +11,13 @@ def unset_is_visible(self, request, queryset):
     queryset.update(is_visible=False)
 unset_is_visible.short_description = '크롤링, 공개 해제'
 
+def delete_recent_in_stock_date(self, request, queryset):
+    queryset.update(recent_in_stock_date=None)
+delete_recent_in_stock_date.short_description = '최근 재고 시간 삭제'
+
 
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = ("product_title", "crawling_type", "now_in_stock", "recent_in_stock_date", "is_visible", "modified_at", )
 
-    actions = [set_is_visible, unset_is_visible]
+    actions = [set_is_visible, unset_is_visible, delete_recent_in_stock_date]
