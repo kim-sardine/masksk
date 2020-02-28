cad293caa363266e6dd02bfd31eed1204298ee58
kim-sardine <kim.sardine@gmail.com>
2020-02-28 14:28:23 +0900

    Add admin action

--- mask/stores/admin.py
+++ mask/stores/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from mask.stores.models import Store
 
+
+def set_is_visible(self, request, queryset):
+    queryset.update(is_visible=True)
+set_is_visible.short_description = '크롤링, 공개 설정'
+
+def unset_is_visible(self, request, queryset):
+    queryset.update(is_visible=False)
+unset_is_visible.short_description = '크롤링, 공개 해제'
+
+
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     list_display = ("product_title", "crawling_type", "now_in_stock", "recent_in_stock_date", "is_visible", "modified_at", )
+
+    actions = [set_is_visible, unset_is_visible]
